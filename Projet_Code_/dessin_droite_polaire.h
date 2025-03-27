099356abcdda40e235d9643d2f16c7b5d42f630f
FatimaZahra00126 <bouzagoufatimazahra@gmail.com>
2025-03-27 20:48:24 +0100

    Delete Projet_Code_ directory
    supprimer tout pour recommencer et faire quelque chose qui marche, je n'arrive pas à faire des modification sur visual studio. du coup je vais essayer de reprendre a zéro

--- Projet_Code_/dessin_droite_polaire.h
+++
@@ -1,9 +0,0 @@
-#ifndef DESSIN_DROITE_POLAIRE_H
-#define DESSIN_DROITE_POLAIRE_H
-
-#include "hough_polaire.h"
-
-// Crée une image avec les droites détectées tracées en rouge
-Image dessiner_droites_polaire(const HoughPolaire& hough, const Image& image_binaire, int seuil);
-#endif
-
--- Projet_Code_/utils_image.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_IMAGE_H
-#define UTILS_IMAGE_H
-
-#include "lecteur_ppm.h"
-#include <vector>
-
-std::vector<std::pair<int, int>> extraire_pixels_blancs(const Image& image_binaire);
-
-#endif
