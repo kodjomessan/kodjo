57cb526e4fa9eb03772ce32e5f931e5d0dcc597a
FatimaZahra00126 <bouzagoufatimazahra@gmail.com>
2025-03-27 22:01:57 +0100

    Delete ProjetExecution directory
    supprimer tt cela ne marche chez moi j'arrive pas a modifier sur localement

--- ProjetExecution/dessin_droite.h
+++
@@ -1,9 +0,0 @@
-#ifndef DESSIN_DROITE_H
-#define DESSIN_DROITE_H
-
-#include "hough_naif.h"
-#include "lecteur_ppm.h"
-
-Image dessiner_droites_naives(const HoughNaif& hough, Image image, int seuil);
-
-#endif
