d1cb8293410473f7b9ab0edcea774b691202998d
FatimaZahra00126 <bouzagoufatimazahra@gmail.com>
2025-03-28 13:08:08 +0100

    Delete ProjetExecution directory

--- ProjetExecution/conversion_binaire.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONVERSION_BINAIRE_H
-#define CONVERSION_BINAIRE_H
-
-#include "lecteur_ppm.h"
-
-Image convertir_en_binaire(const Image& image_entree, int seuil = 128);
-
-#endif
-
--- ProjetExecution/dessin_droite_polaire.h
+++
@@ -1,9 +0,0 @@
-#ifndef DESSIN_DROITE_POLAIRE_H
-#define DESSIN_DROITE_POLAIRE_H
-
-#include "hough_polaire.h"
-#include "lecteur_ppm.h"
-
-Image dessiner_droites_polaire(const HoughPolaire& hough, Image image, int seuil);
-
-#endif
